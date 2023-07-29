d28427cbf9292ddba9e1add70e2cf91f92c2ecde
Khatima Sadat <khatimasadat70@gmail.com>
2023-07-29 08:59:55 +0430

    Routing problem of App, Header, LogIn SignUp fixed

--- react-project/src/App/screens/Login/LogIn.js
+++ react-project/src/App/screens/Login/LogIn.js
@@ -2,40 +2,36 @@ import Spinner from "react-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { useContext } from "react";
 import { Link, useNavigate, useNavigation } from "react-router-dom";
-import i18n from '../../../i18n';
-import { withNamespaces } from 'react-i18next';
-import "./LogIn.css"
+import i18n from "../../../i18n";
+import { withNamespaces } from "react-i18next";
+import "./LogIn.css";
 import { FaGoogle } from "react-icons/fa";
 import { useState, useEffect } from "react";
-import img1 from '../../assets/mg.png';
-import React from 'react'
-import img3 from '../../assets/logo_3.png'
+import img1 from "../../assets/mg.png";
+import React from "react";
+import img3 from "../../assets/logo_3.png";
 // import React from 'react'
 <link rel="stylesheet" href="LogIn.css" />;
 
-
-
-
-
-const LogIn = ({t}, props) => {
+const LogIn = ({ t }, props) => {
   // const login = useGoogleLogin({
-//     onSuccess: async respose => {
-//         try {
-//             const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
-//                 headers: {
-//                     "Authorization": `Bearer ${respose.access_token}`
-//                 }
-//             })
+  //     onSuccess: async respose => {
+  //         try {
+  //             const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+  //                 headers: {
+  //                     "Authorization": `Bearer ${respose.access_token}`
+  //                 }
+  //             })
 
-//             console.log(res.data)
-//         } catch (err) {
-//             console.log(err)
+  //             console.log(res.data)
+  //         } catch (err) {
+  //             console.log(err)
 
-//         }
+  //         }
 
-//     }
-// });
-  const { isLoading, login } = useContext(AuthContext);
+  //     }
+  // });
+  const { isLoading, login, userInfo } = useContext(AuthContext);
   const initialValues = {
     email: "",
     password: "",
@@ -53,11 +49,10 @@ const LogIn = ({t}, props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormErrors(validate(formValues));
-    console.log( formValues.email, formValues.password);
-    login( formValues.email, formValues.password);
+    console.log(formValues.email, formValues.password);
+    login(formValues.email, formValues.password);
     setIsSubmit(true);
   };
-
 
   // const initialValues = { email: "", password: "", };
   // const [formValues, setFormValues] = useState(initialValues);
@@ -91,163 +86,190 @@ const LogIn = ({t}, props) => {
       errors.email = <>{t("login10")}</>;
     }
     if (!values.password) {
-      errors.password =  <>{t("login11")}</>;
+      errors.password = <>{t("login11")}</>;
     } else if (values.password.length < 8) {
-      errors.password =  <>{t("login12")}</>;
+      errors.password = <>{t("login12")}</>;
     }
-    
+
     return errors;
   };
-  return (
-    <div>
-      
-<div id="all_login" >
-<div id="svg_login">
-		<svg class="Path_login" viewBox="0 0 1921.728 880.675">
-			<linearGradient id="Path_login" spreadMethod="pad" x1="0.835" x2="0.274" y1="0" y2="1.305">
-				<stop offset="0" stop-color="#3c98bd" stop-opacity="1"></stop>
-				<stop offset="1" stop-color="#0f53a1" stop-opacity="1"></stop>
-			</linearGradient>
-			<path id="Path_login" d="M 1921.7275390625 965.675048828125 L 403.7140502929688 965.675048828125 C 174.1283569335938 937.8496704101562 0 718.1124877929688 0 456.2157287597656 L 0 0 L 2721.7275390625 0 L 2721.7275390625 965.675048828125 Z">
-			</path>
-		</svg>
-    <img className='img_login' src={img1}/>
-		<div id="Group_login">
-		</div>
-	</div>
-<button className="button_login">
-<div className="upp_login" >{t("login1")}</div>
-  <Link to="/" className="inn_login">{t("login2")}</Link>
-</button>
-<div className="cantainer_login">
-      {Object.keys(formErrors).length === 0 && isSubmit ? (
-        <div className="ui message success">{navigate('/header')}</div>
-      ) : (
-        <pre></pre>
-      )}
+  if (userInfo === null) {
+    return (
+      <div>
+        <div id="all_login">
+          <div id="svg_login">
+            <svg class="Path_login" viewBox="0 0 1921.728 880.675">
+              <linearGradient
+                id="Path_login"
+                spreadMethod="pad"
+                x1="0.835"
+                x2="0.274"
+                y1="0"
+                y2="1.305"
+              >
+                <stop offset="0" stop-color="#3c98bd" stop-opacity="1"></stop>
+                <stop offset="1" stop-color="#0f53a1" stop-opacity="1"></stop>
+              </linearGradient>
+              <path
+                id="Path_login"
+                d="M 1921.7275390625 965.675048828125 L 403.7140502929688 965.675048828125 C 174.1283569335938 937.8496704101562 0 718.1124877929688 0 456.2157287597656 L 0 0 L 2721.7275390625 0 L 2721.7275390625 965.675048828125 Z"
+              ></path>
+            </svg>
+            <img className="img_login" src={img1} />
+            <div id="Group_login"></div>
+          </div>
+          <button className="button_login">
+            <div className="upp_login">{t("login1")}</div>
+            <Link to="/" className="inn_login">
+              {t("login2")}
+            </Link>
+          </button>
+          <div className="cantainer_login">
+            {Object.keys(formErrors).length === 0 && isSubmit ? (
+              <div className="ui message success">{navigate("/header")}</div>
+            ) : (
+              <pre></pre>
+            )}
 
-      <form onSubmit={handleSubmit}>
-        <div className="ui divider"></div>
-        <div className="ui form">
-          <div className="field">
-            <input
-            id="input_login"
-              type="text"
-              name="email"
-              placeholder={t("login3")}
-              value={formValues.email}
-              onChange={(e) => handleChange(e)}
-            />
-            <p  className="error_login">{formErrors.email}</p>
+            <form onSubmit={handleSubmit}>
+              <div className="ui divider"></div>
+              <div className="ui form">
+                <div className="field">
+                  <input
+                    id="input_login"
+                    type="text"
+                    name="email"
+                    placeholder={t("login3")}
+                    value={formValues.email}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <p className="error_login">{formErrors.email}</p>
+                </div>
+
+                <div className="field">
+                  <input
+                    id="input_login"
+                    type="password"
+                    name="password"
+                    placeholder={t("login4")}
+                    value={formValues.password}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <p className="error_login">{formErrors.password}</p>
+                </div>
+                <Link className="forgotpassword_login" to="/forgotpassword">
+                  {" "}
+                  {t("login5")}
+                </Link>
+              </div>
+              <button className="acount_button_login">{t("login6")}</button>
+            </form>
+            <h4 className="p_login">{t("login7")} </h4>
+            <Link to="google.com">
+              <button className="google_button_singup">
+                <FaGoogle className="google_icon_login" />
+                <div className="google_p_login">{t("login8")}</div>
+              </button>
+            </Link>
           </div>
-          
-          <div className="field">
-            <input
-            id="input_login"
-              type="password"
-              name="password"
-              placeholder={t("login4")}
-              value={formValues.password}
-              onChange={(e) => handleChange(e)}
-            />
-            <p  className="error_login">{formErrors.password}</p>
-          </div>
-          <Link className="forgotpassword_login" to="/forgotpassword"> {t("login5")}</Link>
         </div>
-         <button className="acount_button_login">{t("login6")}</button>
-      </form>
-      <h4 className="p_login">{t("login7")} </h4>
-      <Link to="google.com">
-      <button className="google_button_singup">
-      <FaGoogle className="google_icon_login" />
-        <div className="google_p_login">{t("login8")}</div>
-        
-
-</button>
-  
-      </Link>
-      
-    </div>
-</div>
-<div className='Mobile_lr'>
- <div className="M-img">
-<div id="M-arrow">
- <Link to="/SingUp"> <svg xmlns="http://www.w3.org/2000/svg" width={16} height={10} 
-//  {...props} 
- className='M-arrow'>
-    <defs>
-      <clipPath id="a">
-        <path fill="#fff" d="M0 0h16v10H0z" data-name="Rectangle 85" />
-      </clipPath>
-    </defs>
-    <g clipPath="url(#a)" data-name="Group 158">
-      <path
-        fill="#fff"
-        fillRule="evenodd"
-        d="m3.247 6 2.48 2.294a.99.99 0 0 1 0 1.414 1.023 1.023 0 0 1-1.432 0L.445 6.059A1.465 1.465 0 0 1 0 5a1.483 1.483 0 0 1 .444-1.06L4.294.293a1.025 1.025 0 0 1 1.434 0 .992.992 0 0 1 0 1.414L3.248 4H15a1 1 0 0 1 0 2H3.247Z"
-        data-name="Path 433"
-      />
-    </g>
-  </svg></Link>
-  <img src={img3} className="img3"/>
- </div>
-</div>
-<div id='Mobile-bottom'>
- <button className="button_login">
-<div className="upp_login" >{t("login1")}</div>
-  <Link to="/" className="inn_login">{t("login2")}</Link>
-</button>
-<div className="cantainer_login">
-      {Object.keys(formErrors).length === 0 && isSubmit ? (
-        <div className="ui message success">{navigate('/header')}</div>
-      ) : (
-        <pre></pre>
-      )}
-
-      <form onSubmit={handleSubmit}>
-        <div className="ui divider"></div>
-        <div className="ui form">
-          <div className="field">
-            <input
-            id="input_login"
-              type="text"
-              name="email"
-              placeholder={t("login3")}
-              value={formValues.email}
-              onChange={handleChange}
-            />
-            <p  className="error_login">{formErrors.email}</p>
+        <div className="Mobile_lr">
+          <div className="M-img">
+            <div id="M-arrow">
+              <Link to="/SingUp">
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={10}
+                  //  {...props}
+                  className="M-arrow"
+                >
+                  <defs>
+                    <clipPath id="a">
+                      <path
+                        fill="#fff"
+                        d="M0 0h16v10H0z"
+                        data-name="Rectangle 85"
+                      />
+                    </clipPath>
+                  </defs>
+                  <g clipPath="url(#a)" data-name="Group 158">
+                    <path
+                      fill="#fff"
+                      fillRule="evenodd"
+                      d="m3.247 6 2.48 2.294a.99.99 0 0 1 0 1.414 1.023 1.023 0 0 1-1.432 0L.445 6.059A1.465 1.465 0 0 1 0 5a1.483 1.483 0 0 1 .444-1.06L4.294.293a1.025 1.025 0 0 1 1.434 0 .992.992 0 0 1 0 1.414L3.248 4H15a1 1 0 0 1 0 2H3.247Z"
+                      data-name="Path 433"
+                    />
+                  </g>
+                </svg>
+              </Link>
+              <img src={img3} className="img3" />
+            </div>
           </div>
-          
-          <div className="field">
-            <input
-            id="input_login"
-              type="password"
-              name="password"
-              placeholder={t("login4")}
-              value={formValues.password}
-              onChange={handleChange}
-            />
-            <p  className="error_login">{formErrors.password}</p>
+          <div id="Mobile-bottom">
+            <button className="button_login">
+              <div className="upp_login">{t("login1")}</div>
+              <Link to="/" className="inn_login">
+                {t("login2")}
+              </Link>
+            </button>
+            <div className="cantainer_login">
+              {Object.keys(formErrors).length === 0 && isSubmit ? (
+                <div className="ui message success">{navigate("/header")}</div>
+              ) : (
+                <pre></pre>
+              )}
+
+              <form onSubmit={handleSubmit}>
+                <div className="ui divider"></div>
+                <div className="ui form">
+                  <div className="field">
+                    <input
+                      id="input_login"
+                      type="text"
+                      name="email"
+                      placeholder={t("login3")}
+                      value={formValues.email}
+                      onChange={handleChange}
+                    />
+                    <p className="error_login">{formErrors.email}</p>
+                  </div>
+
+                  <div className="field">
+                    <input
+                      id="input_login"
+                      type="password"
+                      name="password"
+                      placeholder={t("login4")}
+                      value={formValues.password}
+                      onChange={handleChange}
+                    />
+                    <p className="error_login">{formErrors.password}</p>
+                  </div>
+                  <Link className="forgotpassword_login" to="/forgotpassword">
+                    {" "}
+                    {t("login5")}
+                  </Link>
+                </div>
+                <button className="acount_button_login">{t("login6")}</button>
+              </form>
+              <h4 className="p_login">{t("login7")} </h4>
+              <Link to="google.com">
+                <button className="google_button_login">
+                  <FaGoogle className="google_icon_login" />
+                  <div className="google_p_login">{t("login8")}</div>
+                </button>
+              </Link>
+            </div>
           </div>
-          <Link className="forgotpassword_login" to="/forgotpassword"> {t("login5")}</Link>
         </div>
-         <button className="acount_button_login">{t("login6")}</button>
-      </form>
-      <h4 className="p_login">{t("login7")} </h4>
-      <Link to="google.com">
-      <button className="google_button_login">
-      <FaGoogle className="google_icon_login" />
-        <div className="google_p_login">{t("login8")}</div>
-</button>
-      </Link>
-    </div>
- </div>
-
-</div>
-
-    </div>
-  )
-}
-export default  withNamespaces()(LogIn) ;
-
+      </div>
+    );
+  } else if (userInfo !== null) {
+    return navigate("/header");
+  } else if (isLoading) {
+    return <h1>Loading.....</h1>;
+  }
+};
+export default withNamespaces()(LogIn);
--- react-project/src/App/screens/SingUp/SingUp.js
+++ react-project/src/App/screens/SingUp/SingUp.js
@@ -1,7 +1,7 @@
-import {GoogleLogin} from '@react-oauth/google';
+import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
-import {useGoogleLogin} from '@react-oauth/google';
-import axios from "axios"
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 // import { Formik, Field } from "formik";
 import * as Yup from "yup";
 // import CustomText from "../../CustomText";
@@ -9,48 +9,47 @@ import Spinner from "react-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { useContext } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import i18n from '../../../i18n';
-import { withNamespaces } from 'react-i18next';
+import i18n from "../../../i18n";
+import { withNamespaces } from "react-i18next";
 // import Translation from '../../../db.json';
 import "./SingUp.css";
 import { FaGoogle } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import img1 from "../../assets/mg.png";
 import React from "react";
-import { Link, useNavigate, useNavigation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 <link rel="stylesheet" href="SingUp.css" />;
 
 const SingUp = ({ t }) => {
- 
-// var t;
+  // var t;
   const login = useGoogleLogin({
-    onSuccess: async respose => {
-        try {
-            const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
-                headers: {
-                    "Authorization": `Bearer ${respose.access_token}`
-                }
-            })
+    onSuccess: async (respose) => {
+      try {
+        const res = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${respose.access_token}`,
+            },
+          }
+        );
 
-            console.log(res.data)
-        } catch (err) {
-            console.log(err)
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
 
-        }
-
-    }
-});
-
-// const { i18n } = this.props;
-// const changeLanguage = (lng) => {
-//     i18n.changeLanguage(lng);
-// };
-  const { isLoading, register } = useContext(AuthContext);
- 
+  // const { i18n } = this.props;
+  // const changeLanguage = (lng) => {
+  //     i18n.changeLanguage(lng);
+  // };
+  const { isLoading, register, userInfo } = useContext(AuthContext);
 
   const changeLanguage = (lng) => {
-		i18n.changeLanguage(lng);
-	  }
+    i18n.changeLanguage(lng);
+  };
 
   const initialValues = {
     username: "",
@@ -86,59 +85,65 @@ const SingUp = ({ t }) => {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
     if (!values.username) {
-      errors.username = <>{t('singup9')}</>;
+      errors.username = <>{t("singup9")}</>;
     }
     if (!values.email) {
-      errors.email =  <>{t('singup10')}</>;
+      errors.email = <>{t("singup10")}</>;
     } else if (!regex.test(values.email)) {
-      errors.email =  <>{t('singup11')}</>;
+      errors.email = <>{t("singup11")}</>;
     }
     if (!values.password) {
-      errors.password = <>{t('singup12')}</>;
+      errors.password = <>{t("singup12")}</>;
     } else if (values.password.length < 8) {
-      errors.password = <>{t('singup13')}</>;
+      errors.password = <>{t("singup13")}</>;
     }
     if (!values.confirmpassword) {
-
-      errors.confirmpassword = <>{t('singup14')}</>;
+      errors.confirmpassword = <>{t("singup14")}</>;
     } else if (values.confirmpassword !== values.password) {
-      errors.confirmpassword =
-      <>{t("singup15")}</>;
-
-
-
+      errors.confirmpassword = <>{t("singup15")}</>;
     }
 
     return errors;
   };
 
-  return (
-    <div>
-      <div id="all_singup">
-        <div id="Group_singup">
-          <svg class="Path_singup" viewBox="0 0 1921.728 880.675">
-            <linearGradient
-              id="Path_singup"
-              spreadMethod="pad"
-              x1="0.835"
-              x2="0.274"
-              y1="0"
-              y2="1.305"
-            >
-              <stop offset="0" stop-color="#3c98bd" stop-opacity="1"></stop>
-              <stop offset="1" stop-color="#0f53a1" stop-opacity="1"></stop>
-            </linearGradient>
-            <path
-              id="Path_singup"
-              d="M 1921.7275390625 965.675048828125 L 403.7140502929688 965.675048828125 C 174.1283569335938 937.8496704101562 0 718.1124877929688 0 456.2157287597656 L 0 0 L 2721.7275390625 0 L 2721.7275390625 965.675048828125 Z"
-            ></path>
-          </svg>
+  if (userInfo === null) {
+    return (
+      <div>
+        <div id="all_singup">
+          <div id="Group_singup">
+            <svg class="Path_singup" viewBox="0 0 1921.728 880.675">
+              <linearGradient
+                id="Path_singup"
+                spreadMethod="pad"
+                x1="0.835"
+                x2="0.274"
+                y1="0"
+                y2="1.305"
+              >
+                <stop offset="0" stop-color="#3c98bd" stop-opacity="1"></stop>
+                <stop offset="1" stop-color="#0f53a1" stop-opacity="1"></stop>
+              </linearGradient>
+              <path
+                id="Path_singup"
+                d="M 1921.7275390625 965.675048828125 L 403.7140502929688 965.675048828125 C 174.1283569335938 937.8496704101562 0 718.1124877929688 0 456.2157287597656 L 0 0 L 2721.7275390625 0 L 2721.7275390625 965.675048828125 Z"
+              ></path>
+            </svg>
 
-          <img id="imgg_singup" src={img1} />
-          <button className="button_singup" onClick={() => changeLanguage('dari')}>زبان دری</button>
-          <button className="button_singup" onClick={() => changeLanguage('pashto')}>پشتو ژبه</button>
-          
-          {/* <button className="button_singup" onClick={() => handleClick("dari")}>
+            <img id="imgg_singup" src={img1} />
+            <button
+              className="button_singup"
+              onClick={() => changeLanguage("dari")}
+            >
+              زبان دری
+            </button>
+            <button
+              className="button_singup"
+              onClick={() => changeLanguage("pashto")}
+            >
+              پشتو ژبه
+            </button>
+
+            {/* <button className="button_singup" onClick={() => handleClick("dari")}>
             زبان دری
           </button>
           <button
@@ -147,80 +152,78 @@ const SingUp = ({ t }) => {
           >
             پشتو ژبه
           </button> */}
-        </div>
-        <button id="login-button">
-          <Link className="up_singup" to="/login">
-          {t('singup1')}
-          </Link>
-          <div className="in_singup">{t('singup2')}</div>
-        </button>
-        <div id="cantainer_singup">
-          {Object.keys(formErrors).length === 0 && isSubmit ? (
-            <div>{navigate("/profile")}</div>
-          ) : (
-            <pre></pre>
-          )}
+          </div>
+          <button id="login-button">
+            <Link className="up_singup" to="/login">
+              {t("singup1")}
+            </Link>
+            <div className="in_singup">{t("singup2")}</div>
+          </button>
+          <div id="cantainer_singup">
+            {Object.keys(formErrors).length === 0 && isSubmit ? (
+              <div>{navigate("/profile")}</div>
+            ) : (
+              <pre></pre>
+            )}
 
-
-          <form onSubmit={handleSubmit}>
-            <div>
-              <input
-                id="input_singup"
-                type="text"
-                name="username"
-                placeholder={t('singup3')}
-                value={formValues.username}
-                onChange={(e) => handleChange(e)}
-              />
-              <p className="error_singup">{formErrors.username}</p>
-              <div className="field">
+            <form onSubmit={handleSubmit}>
+              <div>
                 <input
                   id="input_singup"
                   type="text"
-                  name="email"
-                  placeholder={t('singup4')}
-                  value={formValues.email}
+                  name="username"
+                  placeholder={t("singup3")}
+                  value={formValues.username}
                   onChange={(e) => handleChange(e)}
                 />
-                <p className="error_singup">{formErrors.email}</p>
-              </div>
+                <p className="error_singup">{formErrors.username}</p>
+                <div className="field">
+                  <input
+                    id="input_singup"
+                    type="text"
+                    name="email"
+                    placeholder={t("singup4")}
+                    value={formValues.email}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <p className="error_singup">{formErrors.email}</p>
+                </div>
 
-              <div className="field">
-                <input
-                  id="input_singup"
-                  type="password"
-                  name="password"
-                  placeholder={t('singup5')}
-                  value={formValues.password}
-                  onChange={(e) => handleChange(e)}
-                />
-                <p className="error_singup">{formErrors.password}</p>
-              </div>
+                <div className="field">
+                  <input
+                    id="input_singup"
+                    type="password"
+                    name="password"
+                    placeholder={t("singup5")}
+                    value={formValues.password}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <p className="error_singup">{formErrors.password}</p>
+                </div>
 
-              <div className="field">
-                <input
-                  id="input_singup"
-                  type="password"
-                  name="confirmpassword"
-                  placeholder={t('singup6')}
-                  value={formValues.confirmpassword}
-                  onChange={(e) => handleChange(e)}
-                />
-                <p className="error_singup">{formErrors.confirmpassword}</p>
+                <div className="field">
+                  <input
+                    id="input_singup"
+                    type="password"
+                    name="confirmpassword"
+                    placeholder={t("singup6")}
+                    value={formValues.confirmpassword}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <p className="error_singup">{formErrors.confirmpassword}</p>
+                </div>
               </div>
-            </div>
-            <button className="acount_button_singuo">{t('singup1')}</button>
-          </form>
-          <h4 className="p_singup">{t('singup7')}</h4>
-             <button  className="google_button_singup" onClick={login}
-             >
-            {/* <div>{navigate("/header")}</div> */}
-        
+              <button className="acount_button_singuo">{t("singup1")}</button>
+            </form>
+            <h4 className="p_singup">{t("singup7")}</h4>
+            <button className="google_button_singup" onClick={login}>
+              {/* <div>{navigate("/header")}</div> */}
+
               <FaGoogle className="google_icon_singup" />
-              <div className="google_p_singup">{t('singup8')}</div>
-            </button >
-        </div>
-         {/* <GoogleLogin
+              <div className="google_p_singup">{t("singup8")}</div>
+            </button>
+          </div>
+          {/* <GoogleLogin
             className="google_buttton_singup"
                     onSuccess={credentialResponse => {
                     console.log(credentialResponse.credential);
@@ -230,10 +233,14 @@ const SingUp = ({ t }) => {
                     onError={() => {
                     console.log('Login Failed');
                 }}/> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if (isLoading) {
+    return <h1>Loading.....</h1>;
+  } else {
+    return navigate("/header");
+  }
 };
 
-export default  withNamespaces()(SingUp) ;
-
+export default withNamespaces()(SingUp);
